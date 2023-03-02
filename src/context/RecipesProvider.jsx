7eeb2ca53722092ef7f0acc6b154a83294301839
Miguel Raatz <miguelraatz_@hotmail.com>
2023-03-02 16:44:41 -0300

    Requisito 2: Corrigindo erros de lint

--- src/context/RecipesProvider.jsx
+++ src/context/RecipesProvider.jsx
@@ -1,13 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import RecipesContext from './RecipesContext';
 
-function Provider({ children }) {
-
+function RecipesProvider({ children }) {
+  const value = 'Teste';
   return (
-    <RecipesContext.Provider>
+    <RecipesContext.Provider value={ value }>
       {children}
     </RecipesContext.Provider>
-  )
+  );
 }
 
-export default Provider;
+RecipesProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default RecipesProvider;
