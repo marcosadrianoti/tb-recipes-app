ce56d0ccf3b66c68abd561c74d66ad533bd67b3a
LucasCS <lcsvdk@gmail.com>
2023-03-03 15:03:42 -0300

    Requisito 17 e 18: Cobertura de testes e implemetação do footer nos lugares corretos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 
 function Footer() {
+  const history = useHistory();
   return (
     <div data-testid="footer" className="footer">
-      <button>
+      <button
+        onClick={ () => {
+          history.push('/drinks');
+        } }
+      >
         <img src={ drinkIcon } alt="drinkIcon" data-testid="drinks-bottom-btn" />
       </button>
-      <button>
+      <button
+        onClick={ () => {
+          history.push('/meals');
+        } }
+      >
         <img src={ mealIcon } alt="mealIcon" data-testid="meals-bottom-btn" />
       </button>
     </div>
--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Header from '../components/Header';
 
-function Drinks() {
+function Recipes() {
   return (
     <>
       <Header />
@@ -10,5 +10,4 @@ function Drinks() {
     </>
   );
 }
-
-export default Drinks;
+export default Recipes;
