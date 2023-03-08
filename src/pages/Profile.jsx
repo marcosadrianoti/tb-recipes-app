1b87a6ffd1ca334289b4dc1697d2f4c17852105f
cunhavcs <cunha.vcs@gmail.com>
2023-03-08 19:20:28 -0300

    Requisito 57: Implementa os elementos da tela de perfil respeitando os atributos descritos no protótipo

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,6 +6,10 @@ function Profile() {
   return (
     <>
       <Header />
+      <h3 data-testid="profile-email">email do usuário</h3>
+      <button type="button" data-testid="profile-done-btn">Done Recipes</button>
+      <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
+      <button type="button" data-testid="profile-logout-btn">Logout</button>
       <Footer />
     </>
   );
