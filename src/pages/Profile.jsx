91072279d7efa080c7a21185569218a1d0b6180b
cunhavcs <cunha.vcs@gmail.com>
2023-03-08 19:23:32 -0300

    Requisitos 58 e 59: e-mail da pessoa usuária deve estar visível e crias os 3 botões

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,10 +3,12 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Profile() {
+  const user = JSON.parse(localStorage.getItem('user'));
+
   return (
     <>
       <Header />
-      <h3 data-testid="profile-email">email do usuário</h3>
+      <h3 data-testid="profile-email">{user.email}</h3>
       <button type="button" data-testid="profile-done-btn">Done Recipes</button>
       <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
       <button type="button" data-testid="profile-logout-btn">Logout</button>
